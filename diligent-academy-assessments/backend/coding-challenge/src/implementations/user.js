c82b1fdd2556db1500b1ca6f83de71bb3892ef34
Lényi Martin <lenyim00@gmail.com>
2024-08-29 15:34:16 +0200

    Implement the methods

--- diligent-academy-assessments/backend/coding-challenge/src/implementations/user.js
+++ diligent-academy-assessments/backend/coding-challenge/src/implementations/user.js
@@ -1,21 +1,55 @@
 export class User {
     constructor() {
+        /** @member {Book[]} */
         this.books = [];
     }
 
+    /**
+     * Borrow the given book from the library, when the user does not already have it
+     * @param {Book} book
+     * @param {Library} library
+     */
     borrow(book, library) {
-        throw new Error('Not implemented');
+        if(this.hasBook(book)) {
+            return;
+        }
+
+        this.books.push(book);
+        library.removeBook(book);
     }
 
+    /**
+     * Check the user has the given book
+     * @param book the book to check
+     * @return boolean
+     */
     hasBook(book) {
-        throw new Error('Not implemented');
+        return this.books.includes(book);
     }
 
+    /**
+     * Return the given book to the library
+     *
+     * @param {Book} book
+     * @param {Library} library
+     * @returns {boolean}
+     */
     return(book, library) {
-        throw new Error('Not implemented');
+        if(!this.hasBook(book)) {
+            return false;
+        }
+
+        this.books = this.books.filter(b => b !== book);
+        library.addBook(book);
+        return true;
     }
 
+    /**
+     * Return the name of the books borrowed by the user
+     *
+     * @return {string[]} name of the borrowed book
+     */
     getBookNames() {
-        throw new Error('Not implemented');
+        return this.books.map(b => b.name);
     }
 }
